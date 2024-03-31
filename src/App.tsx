ef30a20b0262fece1a72e187d2f662efa651af4a
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-03-31 23:25:48 +0300

    feat: writing markup with JSX

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyButton from"./components/MyButton";
+import AboutPage from"./components/AboutPage";
 
 function App() {
   return(
   <div>
         <h1>Welcome to my app</h1>
-        <MyButton />
+        <MyButton/>
+        <AboutPage/>
       </div>
       );
 }
---
+++ src/components/AboutPage.tsx
@@ -0,0 +1,11 @@
+function AboutPage(){
+    return(
+        <>
+        <h1>About</h1>
+        <p>Hello.<br/>How do you do?</p>
+        </>
+    );
+
+}
+
+export default AboutPage;
