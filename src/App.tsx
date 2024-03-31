d28defe5adf7ab1cab1e5ea7dff0fc89dc373ece
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-04-01 00:03:49 +0300

    feat: adding styles and displaying data

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import MyButton from"./components/MyButton";
 import AboutPage from"./components/AboutPage";
+import Profile from "./components/Profile";
 
 function App() {
   return(
@@ -10,6 +11,7 @@ function App() {
         <h1>Welcome to my app</h1>
         <MyButton/>
         <AboutPage/>
+        <Profile/>
       </div>
       );
 }
