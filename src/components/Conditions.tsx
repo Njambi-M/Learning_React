80955a20cda8649046c7622133c38fde69762e17
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-04-01 08:31:56 +0300

    feat: conditional rendering

---
+++ src/components/Conditions.tsx
@@ -0,0 +1,28 @@
+// let content;
+// if(isLoggedIn){
+//     content = <AdminPanel/>;
+// } else{
+//     content = <LoginForm/>;
+// }
+
+// return(
+//     <div>
+//         {content}
+//     </div>
+// );
+
+// //or:
+
+// <div>
+//     {isLoggedIn? (
+//         <AdminPanel/>
+//     ):(
+//         <LoginForm/>
+//     )}
+// </div>
+
+// //without else:
+
+// <div>
+// {isLoggedIn && <AdminPanel/>}
+// </div>
