64f5018727ce9dd118ab90026b5137c7176665bc
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-04-01 13:53:52 +0300

    feat: props

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import AboutPage from"./components/AboutPage";
 import Profile from "./components/Profile";
 import ShoppingList from './components/ShoppingList';
 import {useState} from "react";
+import Section from "./components/Section";
+import Heading from "./components/Heading";
 
 function App() {
   const[count, setCount] = useState(0);
@@ -24,6 +26,29 @@ function App() {
         <h1>Counters that update separately</h1>
         <MyButton count ={count} onClick={handleClick}/>
         <MyButton count ={count} onClick={handleClick}/>
+
+        <Section>
+
+          <Heading level={1}>Title</Heading>
+          <Section>
+          <Heading level={2}>Heading</Heading>
+          <Heading level={2}>Heading</Heading>
+          <Heading level={2}>Heading</Heading>
+          <Section>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Section>
+          <Heading level={4}>Sub-sub-heading</Heading>
+          <Heading level={5}>Sub-sub-sub-heading</Heading>
+          <Section>
+          <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+          <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
+        </Section>
+        </Section>
+        </Section>
+        </Section>
+        </Section>
       </div>
       
       );
